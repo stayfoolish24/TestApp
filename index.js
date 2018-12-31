d4e143ebf9baa37efa7edff331cbe63c662b6aaf
SeoulStore <stayfoolish24@gmail.com>
2018-12-31 17:10:23 +0900

    index.js 변경

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 /** @format */
 
 import { AppRegistry } from 'react-native'
-import App from './DidThanosKillMe'
+// import App from './DidThanosKillMe'
 // import App from './Lotto'
-// import App from './UnitConverter'
+import App from './UnitConverter'
 import { name as appName } from './app.json'
 
 AppRegistry.registerComponent(appName, () => App)
